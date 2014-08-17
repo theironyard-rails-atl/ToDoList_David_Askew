69ff6aa4227d71526665143b70496ed9aa64181c
David Askew <davidaskew@gmail.com>
2014-08-17 14:02:53 -0400

    Items controller/index view created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'items/index'
+#  get 'items/index'
 
-#  resources :items
+  resources :items
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
