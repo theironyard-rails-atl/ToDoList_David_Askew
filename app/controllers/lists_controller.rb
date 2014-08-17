8e7e5cb75eddbec152f49b18d9d9d8349bf0e979
David Askew <davidaskew@gmail.com>
2014-08-17 16:59:05 -0400

    added a new list form to show view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,7 +2,17 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
   def show
     @list = List.find(params[:id])
   end
+
+  def create
+    list = List.new
+    list.title = params[:title]
+    list.description = params[:description]
+    list.save!
+    redirect_to lists_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :items, :only => [:index]
 
-  resources :lists, :only => [:index, :show]
+  resources :lists, :only => [:index, :show, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
