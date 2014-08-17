aea1f7be340395d9f739f9ae835f2f79b79254c5
David Askew <davidaskew@gmail.com>
2014-08-17 13:14:35 -0400

    Added associate for List/Item

---
+++ db/migrate/20140817165755_add_item_to_list.rb
@@ -0,0 +1,5 @@
+class AddItemToList < ActiveRecord::Migration
+  def change
+    add_column :items, :list_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140817164203) do
+ActiveRecord::Schema.define(version: 20140817165755) do
 
   create_table "items", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140817164203) do
     t.datetime "due_datetime"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "list_id"
   end
 
   create_table "lists", force: true do |t|
