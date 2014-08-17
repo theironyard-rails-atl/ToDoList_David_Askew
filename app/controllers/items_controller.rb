51c3432066f95fd5255814bc609b09b477e7f351
David Askew <davidaskew@gmail.com>
2014-08-17 18:04:48 -0400

    added a form to list#show to add items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,15 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+  
+  def create
+    item = Item.new
+    item.list_id = params[:list_id]
+    item.name = params[:name]
+    item.checked = false
+    item.due_datetime = params[:due_datetime]
+    item.save!
+    redirect_to list_path(item.list_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 #  get 'items/index'
 
-  resources :items, :only => [:index]
+  resources :items, :only => [:index, :create]
 
   resources :lists, :only => [:index, :show, :create]
   # The priority is based upon order of creation: first created -> highest priority.
