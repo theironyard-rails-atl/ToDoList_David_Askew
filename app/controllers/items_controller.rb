e8106d367d58b090347d89a7eea8a2f95a13a4f5
David Askew <davidaskew@gmail.com>
2014-08-18 11:22:00 -0400

    Added a date picker for the due date

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,16 @@ class ItemsController < ApplicationController
     item.list_id = params[:list_id]
     item.name = params[:name]
     item.checked = false
-    item.due_datetime = params[:due_datetime]
+    #item.due_datetime = params[:due_datetime]
+    year   = params[:date][:year]
+    month  = params[:date][:month]
+    day    = params[:date][:day]
+    hour   = params[:date][:hour]
+    minute = params[:date][:minute]
+
+    item.due_datetime = Time.new(year,month,day,hour,minute)
     item.save!
+    #puts params
     redirect_to list_path(item.list_id)
   end
 
